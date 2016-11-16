26c7296b00849671488c1d8ed5acd76f5a4bb07e
Shaun Parkison <shaun.parkison@nerdymind.com>
2016-11-16 08:51:57 -0700

    more work fleshing out the config

--- src/Config/Craft/Config.php
+++ src/Config/Craft/Config.php
@@ -20,37 +20,63 @@ class Config extends BaseConfig
     public function templates()
     {
         $cwd = getcwd();
-        $command = "";
+        $ssh = "";
 
         // Determine if using SSH password, or keyfile
         if ($this->sshKeyFile !== "") {
-
+            $ssh = "-e 'ssh -i " . $this->sshKeyFile . "'";
         } else {
-
+            $ssh = "--rsh=\"sshpass -p '" . $this->sshPass . "' ssh -o StrictHostKeyChecking=no\"";
         }
 
-        $ssh = new Net_SSH2('example.com');
-        if ($ssh->login('root', 'password')) {
-            $this->io->success('Password Login Success!!');
-        }
-
-        $key = new Crypt_RSA();
-        $key->loadKey(file_get_contents('/Users/ME/.ssh/id_rsa'));
-
-        // echo $ssh->exec('pwd');
-        // echo $ssh->getLog();
-
-        if ($ssh->login('root', $key)) {
-            $this->io->success('Login with keyfile success!!');
-        }
-
+        // Inform user of command
+        $title = ucfirst($this->action) . "ing templates...";
+        $this->io->title($title);
 
         // Determine if push or pull
         if ($this->action === 'pull') {
-            $command = "rsync --progress -rlpt --compress --omit-dir-times --delete --exclude-from='{$cwd}/rsync.ignore' ";
+            $command = "rsync {$ssh} --progress -rlpt --compress --omit-dir-times --delete {$this->sshUser}@{$this->host}:/tmp/ {$cwd}/test";
         } else if ($this->action === 'push') {
-            $command = "rsync --progress -rlpt --compress --omit-dir-times --delete --exclude-from='{$cwd}/rsync.ignore' ";
+            $command = "rsync {$ssh} --progress -rlpt --compress --omit-dir-times --delete {$cwd}/test/ {$this->sshUser}@{$this->host}:/tmp";
         }
+
+        if (isset($command)) {
+            exec($command, $output, $exit_code);
+        }
+
+        if (isset($output) && !empty($output)) {
+            foreach ($output as $line) {
+                $this->io->writeln($line);
+            }
+        }
+
+        if(isset($exit_code) && $exit_code == 0) {
+            $this->io->success("Command executed successfully!");
+        } else {
+            $this->io->error("There was an error executing the command. Please check your config and try again");
+        }
+
+    }
+
+    /**
+     * Sync the database
+     */
+    public function database()
+    {
+        //        $ssh = new Net_SSH2('example.com');
+//        if ($ssh->login('root', 'password')) {
+//            $this->io->success('Password Login Success!!');
+//        }
+//
+//        $key = new Crypt_RSA();
+//        $key->loadKey(file_get_contents('/Users/ME/.ssh/id_rsa'));
+//
+//        // echo $ssh->exec('pwd');
+//        // echo $ssh->getLog();
+//
+//        if ($ssh->login('root', $key)) {
+//            $this->io->success('Login with keyfile success!!');
+//        }
     }
 
 }
